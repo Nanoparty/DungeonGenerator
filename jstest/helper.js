6e8a6baf3535e57b025690a449df2aac6a8c196a
Nathan Foote <39596241+Nanoparty@users.noreply.github.com>
2022-12-20 16:49:10 -0500

    random placement generation prototype

---
+++ jstest/helper.js
@@ -0,0 +1,5 @@
+export function donut() {
+  console.log("donut");
+}
+
+donut();
---
+++ jstest/main.js
@@ -0,0 +1,10 @@
+import { donut } from "./helper.js";
+
+// Function to add two variables.
+const add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(4, 6));
+
+donut();
